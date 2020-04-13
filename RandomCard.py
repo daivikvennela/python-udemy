14f446ee64d227fa0197785e678290a4edfef7c4
Daivik Vennela <daivikvennela@Daiviks-MacBook-Pro.local>
2020-04-12 19:55:27 -0700

    cards with (random)

---
+++ RandomCard.py
@@ -0,0 +1,7 @@
+import random
+suits = [ "clubs", "diamonds", "hearts", "spades"]
+faces = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+my_face = random.choices(faces)
+my_suit = random.choices(suits)
+print("Your card is the ", my_face, "of", my_suit )
+
